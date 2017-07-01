cbc7fbfe14344ac8ef5b19308eb74bf114b6a016
Steve Buzonas <steve@fancyguy.com>
2017-07-01 05:31:40 -0400

    ignore transformed code from instrumentation and add cache key generation

--- config/jest/typescriptTransform.js
+++ config/jest/typescriptTransform.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const babelJest = require('babel-jest');
+const crypto = require('crypto');
 const fs = require('fs-extra');
 const path = require('path');
-const babelJest = require('babel-jest');
+
 const paths = require('../paths');
+const THIS_FILE = fs.readFileSync(__filename);
 
 let babel;
 let tsc;
@@ -13,17 +16,18 @@ function transformSupportsFile(filename) {
   return (filename.endsWith('.ts') || filename.endsWith('.tsx'));
 }
 
+if (fs.existsSync(paths.appTsconfig)) {
+  projectConfig = require(paths.appTsconfig);
+}
+
 module.exports = {
-  process(src, filename, config, transformOptions = {}) {
+  process(src, filename, config, transformOptions = {instrument: false}) {
     if (!transformSupportsFile(filename)) {
       return src;
     }
 
     if (!tsc) {
       tsc = require('typescript');
-      if (fs.existsSync(paths.appTsconfig)) {
-          projectConfig = require(paths.appTsconfig);
-      }
     }
 
     if (!babel) {
@@ -40,9 +44,21 @@ module.exports = {
       jsx: tsc.JsxEmit.React,
     }, projectConfig.compilerOptions);
 
+    if (transformOptions.instrument) {
+      delete theseOptions.outDir;
+      delete theseOptions.sourceMap;
+
+      theseOptions.inlineSourceMap = true;
+      theseOptions.inlineSources = true;
+    }
+
     const result = tsc.transpileModule(src, theseOptions, filename, []);
     const jsFile = filename.replace(/\.(ts|tsx)$/, '.js');
-    const outputText = babel.process(result.outputText, jsFile, config, transformOptions);
+
+    const babelTransformOptions = Object.assign({}, transformOptions);
+    babelTransformOptions.instrument = false;
+
+    const outputText = babel.process(result.outputText, jsFile, config, babelTransformOptions);
     const start = outputText.length > 12 ? outputText.substr(1, 10) : '';
 
     const root = require('pkg-dir').sync();
@@ -59,4 +75,19 @@ module.exports = {
 
     return modified;
   },
+  getCacheKey(fileData, filename, configString, options = { instrument: false }) {
+    return crypto.createHash('md5')
+      .update(THIS_FILE)
+      .update('\0', 'utf8')
+      .update(fileData)
+      .update('\0', 'utf8')
+      .update(filename)
+      .update('\0', 'utf8')
+      .update(configString)
+      .update('\0', 'utf8')
+      .update(JSON.stringify(projectConfig), 'utf8')
+      .update('\0', 'utf8')
+      .update(options.instrument ? 'instrument' : '')
+      .digest('hex');
+  }
 };
--- scripts/utils/createJestConfig.js
+++ scripts/utils/createJestConfig.js
@@ -23,7 +23,7 @@ module.exports = (resolve, rootDir, isEjecting) => {
   // TODO: I don't know if it's safe or not to just use / as path separator
   // in Jest configs. We need help from somebody with Windows to determine this.
   const config = {
-    collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+    collectCoverageFrom: ['src/**/*.{ts,tsx}', '!**/*.d.ts'],
     setupFiles: [resolve('config/polyfills.js')],
     setupTestFrameworkScriptFile: setupTestsFile,
     testMatch: [
